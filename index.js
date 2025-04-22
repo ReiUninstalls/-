684f4c8d7b87c46e67c9a1f95e9c797ad4a29207
ReiUninstalls <reireinstalls@gmail.com>
2025-04-21 21:29:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [427520, 2246340, 489830, 374320];  // Enter here AppIDs of the needed games
+var games = [427520, 489830];  // Enter here AppIDs of the needed games
 var status = 7;  // 1 - online, 7 - invisible
 
 
